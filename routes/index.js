1de78bc2a1bd8c0596090b7414529d29341585a9
SilentCode1003 <j0s3ph0r3nc10@gmail.com>
2023-07-05 09:44:45 +0800

    Added important panels

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", {
+    title: '5L Cart',
+    fullname: 'DEV 42',
+    roletype: 'Admin',
+    position: 'Developer',
+    department: 'Research & Development',
+  });
 });
 
 module.exports = router;
